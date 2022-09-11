5654bf593d9e6c71deb881c86af029d990c3d752
YTim <yun2193@hanmail.net>
2022-09-11 17:11:58 +0900

    홀수 개수 UI 추가
    input range 추가

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById("btn");
 const p = document.getElementById("p");
+const range = document.getElementById("oddCount");
+const oddText = document.getElementById("oddP");
 
 function click() {
   let arr = [];
@@ -21,7 +23,22 @@ function click() {
     if (a > b) return 1;
     else return -1;
   });
+
   p.innerText = `${arr[0]},   ${arr[1]},  ${arr[2]},   ${arr[3]},   ${arr[4]},   ${arr[5]}`;
 }
 
+function handleRangeChange(event) {
+  const { value } = event.target;
+  if (value == 0) {
+    oddText.innerText = "0개 (모두 짝수)";
+  } else if (value == 6) {
+    oddText.innerText = "6개 (모두 홀수)";
+  } else if (value == 7) {
+    oddText.innerText = "모두 랜덤";
+  } else {
+    oddText.innerText = `${value}개`;
+  }
+}
+
 btn.addEventListener("click", click);
+range.addEventListener("input", handleRangeChange);
